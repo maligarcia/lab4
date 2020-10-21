e7232fb164d3888b76b24994f124b1263162ead3
Mali <mali.garcia@pcc.edu>
2020-10-20 22:23:32 -0700

    added print date

--- src/edu/pcc/cis233j/lab4/lab4.java
+++ src/edu/pcc/cis233j/lab4/lab4.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis233j.lab4;
 
+import java.util.Date;
+
 /**
  * Created by Mali Garcia on 10/20/20
  */
@@ -8,5 +10,6 @@ public class lab4 {
 
     public static void main(String[] args){
         System.out.println("Hello, World");
+        System.out.println(new Date());
     }
 }
