839f185b07c68152830ea9e490102695ad9d62da
Mali <mali.garcia@pcc.edu>
2020-10-20 22:19:19 -0700

    added main method that prints hellow world

--- src/edu/pcc/cis233j/lab4/lab4.java
+++ src/edu/pcc/cis233j/lab4/lab4.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis233j.lab4;
 
+/**
+ * Created by Mali Garcia on 10/20/20
+ */
+
 public class lab4 {
+
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
 }
